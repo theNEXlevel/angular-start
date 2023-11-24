ae0e10cff7c63d9c601f2774664dc30b77febccf
Justin McLellan <justin.mclellan99@gmail.com>
2023-11-24 15:58:14 -0600

    Initial setup for todo app

--- apps/ui/src/app/app.component.ts
+++ apps/ui/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'angular-start-root',
+  imports: [RouterModule],
+  selector: 'as-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
