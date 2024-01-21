0f08a03e43e8d2f1569cdc953f7ebcee9e0366ca
Justin McLellan <justin.mclellan99@gmail.com>
2024-01-21 15:44:34 -0600

    add firebase and start script

--- apps/chat/src/app/app.config.ts
+++ apps/chat/src/app/app.config.ts
@@ -1,7 +1,41 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, InjectionToken } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { Firestore, initializeFirestore, connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { environment } from '../environments/environment';
+
 import { appRoutes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+
+const app = initializeApp(environment.firebase);
+
+export const AUTH = new InjectionToken('Firebase auth', {
+  providedIn: 'root',
+  factory: () => {
+    const auth = getAuth();
+    if (environment.useEmulators) {
+      connectAuthEmulator(auth, 'http://localhost:9099', {
+        disableWarnings: true,
+      });
+    }
+    return auth;
+  },
+});
+
+export const FIRESTORE = new InjectionToken('Firebase firestore', {
+  providedIn: 'root',
+  factory: () => {
+    let firestore: Firestore;
+    if (environment.useEmulators) {
+      firestore = initializeFirestore(app, {});
+      connectFirestoreEmulator(firestore, 'localhost', 8080);
+    } else {
+      firestore = getFirestore();
+    }
+    return firestore;
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(appRoutes), provideAnimations()],
--- apps/chat/src/environments/environment.development.ts
+++ apps/chat/src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'demo-project',
+    appId: '1:960305453784:web:079881442103e0a7fdd5ad',
+    storageBucket: 'chat-6f1f1.appspot.com',
+    apiKey: 'AIzaSyBQqJNYoG32IXhJ6_LlSu7Ke5EfPJbxXqc',
+    authDomain: 'chat-6f1f1.firebaseapp.com',
+    messagingSenderId: '960305453784',
+  },
+  production: false,
+  useEmulators: true,
+};
